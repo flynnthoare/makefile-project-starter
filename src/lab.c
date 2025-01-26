0b434e9f3c9c672aa7c508e32331ac1e703691ec
Flynn <flynnhoare@u.boisestate.edu>
2025-01-26 15:38:44 -0700

    finished

--- src/lab.c
+++ src/lab.c
@@ -1,1 +1,183 @@
-/**Update this file with the starter code**/
+#include <stdlib.h>
+#include <stdio.h>
+#include "lab.h"
+
+static void destroy_data(void *data)
+{
+  free(data);
+}
+
+static int compare_to(const void *a, const void *b)
+{
+  int fst = *(int *)a;
+  int snd = *(int *)b;
+  return fst - snd;
+}
+
+
+/**
+    * @brief Create a new list with callbacks that know how to deal with the data that
+    * list is storing. The caller must pass the list to list_destroy when finished to
+    * free any memory that was allocated.
+    *
+    * @param destroy_data Function that will free the memory for user supplied data
+    * @param compare_to Function that will compare two user data elements
+    * @return struct list* pointer to the newly allocated list.
+    */
+list_t *list_init(void (*destroy_data)(void *), int (*compare_to)(const void *, const void *))
+{
+    list_t *list = (list_t *)malloc(sizeof(list_t));
+    if (list == NULL) {
+        return NULL;
+    }
+
+    //assign functions
+    list->destroy_data = destroy_data;
+    list->compare_to = compare_to;
+
+    //init sentinel node
+    list->head = (node_t *)malloc(sizeof(node_t));
+    list->head->data = NULL;
+    list->head->next = list->head;
+    list->head->prev = list->head;
+
+    list->size = 0;
+
+    return list;
+}
+
+/**
+* @brief Destroy the list and and all associated data. This functions will call
+* destroy_data on each nodes data element.
+*
+* @param list a pointer to the list that needs to be destroyed
+*/
+void list_destroy(list_t **list)
+{
+    if (list == NULL || *list == NULL) {
+        return;
+    }
+
+    //set current to node after sentinel node
+    node_t *current = (*list)->head->next;
+
+    while(current != (*list)->head) {
+        //store next node
+        node_t *next = current->next;
+        //free the nodes data
+        if((*list)->destroy_data) {
+            (*list)->destroy_data(current->data);
+        }
+        //free the node itself
+        free(current);
+        //move to next node
+        current = next;
+    }
+
+    //free sentinel node
+    free((*list)->head);
+
+    //free actual list
+    free(*list);
+    *list = NULL;
+}
+
+/**
+* Adds data to the front of the list
+*
+* @param list a pointer to an existing list.
+* @param data the data to add
+* @return A pointer to the list
+*/
+list_t *list_add(list_t *list, void *data)
+{
+    //allocate memory for new node
+    node_t *new_node = (node_t *)malloc(sizeof(node_t));
+
+    new_node->data = data;
+
+    new_node->next = list->head->next;
+    new_node->prev = list->head;
+
+    list->head->next->prev = new_node;
+    list->head->next = new_node;
+
+    list->size++;
+
+    return list;
+}
+
+/**
+* @brief Removes the data at the specified index. If index is invalid
+* then this function does nothing and returns NULL
+*
+* @param list The list to remove the element from
+* @param index The index
+* @return void* The data that was removed or NULL if nothing was removed
+*/
+void *list_remove_index(list_t *list, size_t index)
+{
+    //check if list/index is valid
+    if(list == NULL || index>= list->size) {
+        return NULL;
+    }
+
+    //traverse to the node we want to remove
+    node_t *current = list->head->next;
+    for (size_t i = 0; i < index; i++) {
+        current = current->next;
+    }
+
+    //remove the node
+    current->prev->next = current->next;
+    current->next->prev = current->prev;
+
+    if (list->size == 1) {
+        list->head->next = list->head;
+        list->head->prev = list->head;
+    }
+
+    //extract data
+    void *data = current->data;
+
+    //free the node
+    free(current);
+
+    //update list size
+    list->size--;
+
+    return data;
+}
+
+/**
+* @brief Search for any occurrence of data from the list.
+* Internally this function will call compare_to on each item in the list
+* until a match is found or the end of the list is reached. If there are
+* multiple copies of the same data in the list the first one will be returned.
+*
+* @param list the list to search for data
+* @param data the data to look for
+* @return The index of the item if found or -1 if not
+*/
+int list_indexof(list_t *list, void *data)
+{
+    //validate input/list
+    if (list == NULL || list->size == 0) {
+        return -1; 
+    }
+
+    node_t *current = list->head->next;
+    int index = 0;
+
+    while (current != list->head) {
+        
+        if (list->compare_to(current->data, data) == 0) {
+            return index;
+        }
+        current = current->next;
+        index++;
+    }
+
+    //data not found
+    return -1;
+}
--- tests/test-lab.c
+++ tests/test-lab.c
@@ -1,21 +1,326 @@
-#include "harness/unity.h"
+#define UNITY_SUPPORT_64
+#define UNITY_OUTPUT_VERBOSE
+#include "../tests/harness/unity.h"
 #include "../src/lab.h"
 
+static list_t *lst_ = NULL;
+
+static int *alloc_data(int i)
+{
+  int *rval = (int *)malloc(sizeof(int));
+  *rval = i;
+  return rval;
+}
+
+static void destroy_data(void *data)
+{
+  free(data);
+}
+
+static int compare_to(const void *a, const void *b)
+{
+  int fst = *(int *)a;
+  int snd = *(int *)b;
+  return fst - snd;
+}
+
+static void populate_list(void)
+{
+  for (int i = 0; i < 5; i++)
+    {
+      list_add(lst_, alloc_data(i));
+    }
+}
 
 void setUp(void) {
-  // set stuff up here
+  lst_ = list_init(destroy_data, compare_to);
 }
 
 void tearDown(void) {
-  // clean stuff up here
+  list_destroy(&lst_);
 }
 
-void test_stuff(void){
-  // test stuff here
+void test_create_destroy(void)
+{
+  list_t *lst = NULL;
+  lst = list_init(destroy_data, compare_to);
+  TEST_ASSERT_FALSE(lst == NULL);
+  TEST_ASSERT_FALSE(lst->head == NULL);
+  TEST_ASSERT_TRUE(lst->size == 0);
+  TEST_ASSERT_TRUE(lst->head->data == NULL);
+  //Make sure the function pointers are pointing to the correct fuctions
+  TEST_ASSERT_TRUE(lst->destroy_data == destroy_data);
+  TEST_ASSERT_TRUE(lst->compare_to == compare_to);
+
+  //Make sure we are a circular linked list
+  TEST_ASSERT_FALSE(lst->head->next == NULL);
+  TEST_ASSERT_FALSE(lst->head->prev == NULL);
+  TEST_ASSERT_TRUE(lst->head->next == lst->head->prev);
+
+  list_destroy(&lst);
+  TEST_ASSERT_TRUE(lst == NULL);
+}
+
+
+void test_add1(void)
+{
+  list_add(lst_, alloc_data(1));
+  TEST_ASSERT_TRUE(lst_->size == 1);
+  //With one node both next and prev should be equal
+  TEST_ASSERT_TRUE(lst_->head->next == lst_->head->prev);
+  //Make sure we didn't clobber our sentinel node
+  TEST_ASSERT_FALSE(lst_->head == lst_->head->next);
+  TEST_ASSERT_FALSE(lst_->head == lst_->head->prev);
+  TEST_ASSERT_TRUE(lst_->head->data == NULL);
+
+  //Check to make sure our data actually made it into the node
+  TEST_ASSERT_TRUE(*((int *)lst_->head->next->data) == 1);
+  TEST_ASSERT_TRUE(*((int *)lst_->head->prev->data) == 1);
+}
+
+void test_add2(void)
+{
+  list_add(lst_, alloc_data(1));
+  TEST_ASSERT_TRUE(lst_->size == 1);
+  list_add(lst_, alloc_data(2));
+  TEST_ASSERT_TRUE(lst_->size == 2);
+
+  //With two nodes both next and prev should NOT be equal
+  TEST_ASSERT_FALSE(lst_->head->next == lst_->head->prev);
+  //Make sure we didn't clobber our sentinel node
+  TEST_ASSERT_FALSE(lst_->head == lst_->head->next);
+  TEST_ASSERT_FALSE(lst_->head == lst_->head->prev);
+  TEST_ASSERT_TRUE(lst_->head->data == NULL);
+
+  //Check to make sure our next and prev have the correct data
+  TEST_ASSERT_TRUE(*((int *)lst_->head->next->data) == 2);
+  TEST_ASSERT_TRUE(*((int *)lst_->head->prev->data) == 1);
+}
+
+
+void test_removeIndex0(void)
+{
+  populate_list();
+  int *rval = (int *)list_remove_index(lst_, 0);
+  TEST_ASSERT_TRUE(lst_->size == 4);
+  TEST_ASSERT_TRUE(*rval == 4);
+  free(rval);
+
+  node_t *curr = lst_->head->next;
+  //List should be 3->2->1->0
+  for (int i = 3; i >= 0; i--)
+    {
+      TEST_ASSERT_TRUE(*((int *)curr->data) == i);
+      curr = curr->next;
+    }
+  curr = lst_->head->prev;
+  for (int i = 0; i <= 3; i++)
+    {
+      TEST_ASSERT_TRUE(*((int *)curr->data) == i);
+      curr = curr->prev;
+    }
+}
+
+void test_removeIndex3(void)
+{
+  populate_list();
+  int *rval = (int *)list_remove_index(lst_, 3);
+  TEST_ASSERT_TRUE(lst_->size == 4);
+  TEST_ASSERT_TRUE(*rval == 1);
+  free(rval);
+
+  node_t *curr = lst_->head->next;
+  //List should be 4->3->2->0
+  for (int i = 3; i >= 1; i--)
+    {
+      TEST_ASSERT_TRUE(*((int *)curr->data) == i + 1);
+      curr = curr->next;
+    }
+  //Check the last one
+  TEST_ASSERT_TRUE(*((int *)curr->data) == 0);
+
+  //Set the curr back one node so we can check prev links
+  curr = curr->prev;
+  for (int i = 1; i <= 3; i++)
+    {
+      TEST_ASSERT_TRUE(*((int *)curr->data) == i + 1);
+      curr = curr->prev;
+    }
+}
+
+
+void test_removeIndex4(void)
+{
+  populate_list();
+  int *rval = (int *)list_remove_index(lst_, 4);
+  TEST_ASSERT_TRUE(lst_->size == 4);
+  TEST_ASSERT_TRUE(*rval == 0);
+  free(rval);
+
+  node_t *curr = lst_->head->next;
+  //List should be 4->3->2->1
+  for (int i = 3; i >= 0; i--)
+    {
+      TEST_ASSERT_TRUE(*((int *)curr->data) == i + 1);
+      curr = curr->next;
+    }
+  curr = lst_->head->prev;
+  for (int i = 0; i <= 3; i++)
+    {
+      TEST_ASSERT_TRUE(*((int *)curr->data) == i + 1);
+      curr = curr->prev;
+    }
+}
+
+
+void test_invaidIndex(void)
+{
+  populate_list();
+  void *rval = list_remove_index(lst_, 666);
+  TEST_ASSERT_TRUE(rval == NULL);
+  TEST_ASSERT_TRUE(lst_->size == 5);
+
+  node_t *curr = lst_->head->next;
+  //List should be 4->3->2->1->0
+  for (int i = 4; i >= 0; i--)
+    {
+      TEST_ASSERT_TRUE(*((int *)curr->data) ==  i);
+      curr = curr->next;
+    }
+
+  for (int i = 0; i >= 4; i++)
+    {
+      TEST_ASSERT_TRUE(*((int *)curr->data) == i);
+      curr = curr->prev;
+    }
+}
+
+void test_removeAll(void)
+{
+  populate_list();
+  //List should be 4->3->2->1->0
+  for (int i = 4; i >= 0; i--)
+    {
+      int *rval = (int *)list_remove_index(lst_, 0);
+      TEST_ASSERT_TRUE(*rval == i);
+      free(rval);
+    }
+
+  //Make sure we back to default
+  TEST_ASSERT_FALSE(lst_->head->next == NULL);
+  TEST_ASSERT_FALSE(lst_->head->prev == NULL);
+  TEST_ASSERT_TRUE(lst_->head->next == lst_->head->prev);
+  TEST_ASSERT_TRUE(lst_->size == 0);
+}
+
+void test_indexOf0(void)
+{
+  populate_list();
+  //List should be 4->3->2->1->0
+  void *data = lst_->head->next->data;
+  size_t idx = list_indexof(lst_, data);
+  TEST_ASSERT_TRUE(idx == 0);
+}
+
+void test_indexOf3(void)
+{
+  populate_list();
+  //List should be 4->3->2->1->0
+  void *data = alloc_data(1);
+  size_t idx = list_indexof(lst_, data);
+  TEST_ASSERT_TRUE(idx == 3);
+  free(data);
+}
+
+void test_notInList(void)
+{
+  populate_list();
+  void *data = alloc_data(22);
+  int idx = list_indexof(lst_, data);
+  TEST_ASSERT_EQUAL_INT64(-1, idx);
+  free(data);
+}
+
+void test_removeFromEmptyList(void)
+{
+  list_t *list = list_init(destroy_data, compare_to);
+
+  void *data = list_remove_index(list, 0);
+  TEST_ASSERT_TRUE(data == NULL); // No data should be returned
+  TEST_ASSERT_TRUE(list->size == 0); // Size should remain 0
+
+  list_destroy(&list);
+}
+
+void test_addLargeAmountOfData(void)
+{
+  list_t *list = list_init(destroy_data, compare_to);
+
+  // Add 1000 elements
+  for (int i = 0; i < 1000; i++) {
+      list_add(list, alloc_data(i));
+  }
+  TEST_ASSERT_TRUE(list->size == 1000); // Ensure size matches
+
+  // Verify the elements in reverse order (added to front)
+  node_t *current = list->head->next;
+  for (int i = 999; i >= 0; i--) {
+      TEST_ASSERT_TRUE(*(int *)current->data == i);
+      current = current->next;
+  }
+
+  list_destroy(&list);
+}
+
+void test_destroyTwice(void)
+{
+  list_t *list = list_init(destroy_data, compare_to);
+
+  // Add some elements
+  for (int i = 0; i < 3; i++) {
+      list_add(list, alloc_data(i));
+  }
+
+  // Destroy the list
+  list_destroy(&list);
+  TEST_ASSERT_TRUE(list == NULL); // Ensure list is NULL after destroy
+
+  // Attempt to destroy again
+  list_destroy(&list);
+  TEST_ASSERT_TRUE(list == NULL); // Ensure no crashes or changes
+}
+
+void test_removeMiddleNode(void)
+{
+  populate_list(); // Populates list with {4, 3, 2, 1, 0}
+
+  // Remove the middle node (index 2, value 2)
+  void *data = list_remove_index(lst_, 2);
+  TEST_ASSERT_TRUE(*(int *)data == 2); // Ensure correct node was removed
+  free(data);
+
+  // Verify the updated structure
+  TEST_ASSERT_TRUE(lst_->size == 4); // Size should decrease
+  TEST_ASSERT_TRUE(*(int *)lst_->head->next->next->next->data == 1); // Ensure continuity
+  TEST_ASSERT_TRUE(*(int *)lst_->head->next->next->next->prev->data == 3); // Verify prev linkage
 }
 
 int main(void) {
   UNITY_BEGIN();
-  RUN_TEST(test_stuff);
+  RUN_TEST(test_create_destroy);
+  RUN_TEST(test_add1);
+  RUN_TEST(test_add2);
+  RUN_TEST(test_removeIndex0);
+  RUN_TEST(test_removeIndex3);
+  RUN_TEST(test_removeIndex4);
+  RUN_TEST(test_invaidIndex);
+  RUN_TEST(test_removeAll);
+  RUN_TEST(test_indexOf0);
+  RUN_TEST(test_indexOf3);
+  RUN_TEST(test_notInList);
+  RUN_TEST(test_removeFromEmptyList);
+  RUN_TEST(test_addLargeAmountOfData);
+  RUN_TEST(test_destroyTwice);
+  RUN_TEST(test_removeMiddleNode);
   return UNITY_END();
 }
